661a812148e4c545b26aa60d66389f501817b128
Bilal Salha <bsalha1@gmail.com>
2019-12-02 00:51:49 -0500

    Delete ByteClassLoader.java

--- src/com/reliableplugins/loaderclasssender/loader/ByteClassLoader.java
+++
@@ -1,9 +0,0 @@
-package com.reliableplugins.loaderclasssender.loader;
-
-public class ByteClassLoader extends ClassLoader
-{
-    public Class findClass(String name, byte[] data)
-    {
-        return defineClass(name, data,0, data.length);
-    }
-}
